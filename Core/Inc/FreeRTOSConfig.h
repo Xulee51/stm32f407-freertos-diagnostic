09840a3dedf3fb2b9d4112457ae5facfb2b24ff7
Xulee51 <2435695912@qq.com>
2026-08-13 00:03:13 +0800

    fix: defer FreeRTOS tick until scheduler starts

--- Core/Inc/FreeRTOSConfig.h
+++ Core/Inc/FreeRTOSConfig.h
@@ -48,6 +48,4 @@ extern uint32_t SystemCoreClock;
 
 #define vPortSVCHandler                          SVC_Handler
 #define xPortPendSVHandler                       PendSV_Handler
-#define xPortSysTickHandler                      SysTick_Handler
-
 #endif
